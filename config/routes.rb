26e9f38ecc490c7372b3fe9c398dace346586358
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-05 01:42:40 +0000

    [Finish]アプリケーションを完成させよう2-19章

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-   resources :post_comments,only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments,only: [:create, :destroy]
    end
 
    resources :users, only: [:show, :edit, :update]
